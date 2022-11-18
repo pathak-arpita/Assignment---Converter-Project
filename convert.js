83cceed55598cd97ccaede9b8ff01668856f0d82
Arpita <pathak.arpita6497@gmail.com>
2022-11-18 20:59:26 +0530

    initial commit

--- convert.js
+++ convert.js
@@ -1,26 +1,58 @@
-// Accessing All the Elements -
-const textNo = document.getElementById('textNum');
-const  optn = document.getElementById('base');
-const conButn  = document.getElementById('bt-Convert');
-const spanEle = document.getElementById('conv-Value');
+"use strick"
 
-// Add some Event's on convert Button -
-conButn.addEventListener('click', function() {
-      var  num = +textNo.value;   // for converting String -> Number
-      var base = +optn.value;     // for converting String -> Number
+// Accessing All the Elements -
+const num1 = document.getElementById('base-1');           // for selecting ->  FROM options
+const num2 = document.getElementById('base-2');           // for selecting ->  TO options
+const inputNum1 = document.getElementById('text-1')       // for selecting ->  FROM Text Block
+const inputNum2 = document.getElementById('text-2')       // for selecting ->  TO Text Block
+const conBtn = document.getElementById('convert');       // CONVERT Button
+const swapBtn = document.getElementById('swap');             // SWAP Button
+
+
+// Add some Event's on CONVERT Button -
+conBtn.addEventListener('click', function () {
+
+    // 1). For converting DECIMAL to other forms - 
+    var num = +inputNum1.value;   // for converting String -> Number
+    var base = +num2.value;     // for converting String -> Number
 
     // If user enters invalid input(i.e Alphanumeric values) - 
-    if(isNaN(num)) {
-        spanEle.textContent = "Please Enter a valide NUMBER....";
-        spanEle.style.color = '#FF2400';   //For printing that msg in Red color  
-        spanEle.style.fontSize = "30px";
-    }  
-    else{
-       var convertedVal = num.toString(base);  //toString -> will conver num to given base -> return STRING
-       spanEle.textContent = convertedVal.toUpperCase();
-       spanEle.style.color = '#81C784'; 
-       spanEle.style.fontSize = "30px";
-       spanEle.style.marginLeft = "530px";
-       spanEle.style.fontWeight = 'bold';
+    if (isNaN(num)) {
+        inputNum2.textContent = "Please Enter a valide NUMBER....";
+        inputNum2.style.color = '#FF2400';   //For printing that msg in Red color  
+        inputNum2.style.fontSize = "15px";
+    }
+    else {
+        var convertedVal = num.toString(base);  //toString -> will conver num to given base -> return STRING
+        inputNum2.textContent = convertedVal.toUpperCase();
+        inputNum2.style.color = '#81C784';
+        inputNum2.style.fontSize = "15px";
+        //  inputNum2.style.marginLeft = "530px";
+        inputNum2.style.fontWeight = 'bold';
     }
 })
+
+// Add some Event's on SWAP Button -
+swapBtn.addEventListener('click', function () {
+
+    // 1). For converting DECIMAL to other forms - 
+    var num = +inputNum2.value;   // for converting String -> Number
+    var base = +num1.value;     // for converting String -> Number
+
+    // If user enters invalid input(i.e Alphanumeric values) - 
+    if (isNaN(num)) {
+        inputNum1.textContent = "Please Enter a valide NUMBER....";
+        inputNum1.style.color = '#FF2400';   //For printing that msg in Red color  
+        inputNum1.style.fontSize = "15px";
+    }
+    else {
+        var convertedVal = num.toString(base);  //toString -> will conver num to given base -> return STRING
+        inputNum1.textContent = convertedVal.toUpperCase();
+        inputNum1.style.color = '#81C784';
+        inputNum1.style.fontSize = "15px";
+        //  inputNum2.style.marginLeft = "530px";
+        inputNum1.style.fontWeight = 'bold';
+    }
+})
+
+
